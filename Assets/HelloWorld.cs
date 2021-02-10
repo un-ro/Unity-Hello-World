1ef460f94052499a1542d710e6a9aa23f07553c8
Unero <20809454+Unero@users.noreply.github.com>
2021-02-10 11:14:25 +0700

    Project

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld: MonoBehaviour
+{
+    public string myText;
+    // Start is called before the first frame update
+    void Start(){}
+
+    // Update is called once per frame
+    void Update(){}
+
+    void OnGUI(){
+	GUILayout.BeginArea(new Rect(10,10,200,200));
+	GUILayout.Label(myText);
+	GUILayout.EndArea();
+    }
+}
